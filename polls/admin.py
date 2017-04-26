5fd898aaf821665d87b3fdbe871e13333bedefba
Austin Brown <abrown1357@live.com>
2017-04-26 14:39:29 +0000

    finished admin page for now added choices

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Movies
+from .models import Movies, Choice
 
 # Register your models here.
 
 admin.site.register(Movies)
+admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -8,8 +8,16 @@ class Movies(models.Model):
     Movie_Title = models.CharField(max_length=200)
     image = models.ImageField(null=True, blank=True)
     pub_date = models.DateTimeField('date published')
-    
+
+    def __str__(self):
+        return self.Movie_Title
+
 class Choice(models.Model):
     question = models.ForeignKey(Movies, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)  
+    choice_text = models.IntegerField(default=3)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.Movie_Title
+
+    
