39b78c4f838996c008bafbc27f2e05204e54fd14
Austin Brown <abrown1357@live.com>
2017-04-26 14:04:45 +0000

    created a admin page that takes in a title, image, and date/time

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,15 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
+
+class Movies(models.Model):
+    Movie_Title = models.CharField(max_length=200)
+    image = models.ImageField(null=True, blank=True)
+    pub_date = models.DateTimeField('date published')
+    
+class Choice(models.Model):
+    question = models.ForeignKey(Movies, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)  
--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-#this is the index were the 
+#this is the index veiw this is the main site will show the polls.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
