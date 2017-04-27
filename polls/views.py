1058641f5da6fe53ea750767be30105e11e2f61d
Austin Brown <abrown1357@live.com>
2017-04-27 18:26:11 +0000

    got it to display to the index site all the titles of the movies, added css file but nothing in it, so year

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render,get_object_or_404
+from .models import Movies
+from django.template import loader
+from django.core.urlresolvers import reverse
+from django.template.response import TemplateResponse
 
 
-#this is the index veiw this is the main site will show the polls.
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    data=Movies.objects.order_by('-pub_date')
+    return TemplateResponse(request, 'polls/index.html', {"data": data})
+    
+    
+    
+    
+#    movie_list=Movies.objects.order_by('-pub_date')
+# #movie=get_object_or_404(Movies,pk=1)
+#    template=loader.get_template('polls/index.html')
+#    context={
+#        'movie_list': movie_list,
+#    }
+#    return HttpResponse(template.render(context,request))
+   
+
+
+   
+    #return HttpResponse("Hello, world. You're at the polls index.")
+    
+ 
