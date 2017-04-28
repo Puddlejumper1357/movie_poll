f97b2c59c0304fd241f545e4cb3c6b83b98c3cf6
Austin Brown <abrown1357@live.com>
2017-04-28 20:32:48 +0000

    got the average function to work and it to display 5stars images

--- polls/views.py
+++ polls/views.py
@@ -9,6 +9,14 @@ from django.template.response import TemplateResponse
 
 def index(request):
     data=Movies.objects.order_by('-pub_date')
+    
+    
+    for movie in data:
+        if movie.Total_Number_of_Votes==0:
+            movie.avg="No one has voted for this yet be the first"
+        else:
+            movie.avg=movie.Total_Number_of_Stars/movie.Total_Number_of_Votes
+        
     return TemplateResponse(request, 'polls/index.html', {"data": data})
     
     
