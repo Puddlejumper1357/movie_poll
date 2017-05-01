32a3e36ce3cb16ca84380fbfbafce49437e87505
Austin Brown <abrown1357@live.com>
2017-05-01 04:42:36 +0000

    trying to figure out the button never did finishing touches on what I could

--- polls/views.py
+++ polls/views.py
@@ -9,30 +9,22 @@ from django.template.response import TemplateResponse
 
 def index(request):
     data=Movies.objects.order_by('-pub_date')
-    
-    
     for movie in data:
         if movie.Total_Number_of_Votes==0:
             movie.avg="No one has voted for this yet be the first"
         else:
             movie.avg=movie.Total_Number_of_Stars/movie.Total_Number_of_Votes
-        
     return TemplateResponse(request, 'polls/index.html', {"data": data})
-    
-    
-    
-    
-#    movie_list=Movies.objects.order_by('-pub_date')
-# #movie=get_object_or_404(Movies,pk=1)
-#    template=loader.get_template('polls/index.html')
-#    context={
-#        'movie_list': movie_list,
-#    }
-#    return HttpResponse(template.render(context,request))
-   
 
 
-   
-    #return HttpResponse("Hello, world. You're at the polls index.")
+
+def vote(request, pk):  
+    movie=get_object_or_404(Movies, pk=pk)  
+    thisVote=movie.choice_set.get(pk=request.POST['myvote'])
+    movie.Total_Number_of_Stars=thisVote.Total_Number_of_Stars+ thisVote
+    movie.Total_Number_of_Votes += 1
+    movie.save() 
+    return HttpResponseRedirect(reverse('polls:index.html'))
     
+
  
